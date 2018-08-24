79f89c123cb02866e6e7afd95164e7618eb0bd0b
insanj <insanj@gmail.com>
2018-08-24 14:53:40 -0400

    add conda env, use for py, selenium v, attempt refactor, travis.yml

--- Makefile
+++ Makefile
@@ -1,6 +1,21 @@
-
-all:
-	python posty.py
+SHELL=/bin/bash
+ENV=posty
+RUN=posty.py
 
 setup:
-	pip install -U selenium
+	conda env create --file environment.yml
+
+run:
+	source activate $(ENV) && python $(RUN) && source deactivate
+
+clean:
+	conda env remove --name $(ENV) --yes
+	conda clean --source-cache --yes
+
+# Testing
+test: setup run clean
+
+# Installs Anaconda on Linux (good for Ubuntu Subsystem)
+setup-conda:
+	wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
+	bash Anaconda-2.3.0-Linux-x86_64.sh
--- posty.py
+++ posty.py
@@ -5,35 +5,83 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def PostyConfig(Object):
+	exe = os.path.join(os.getcwd(), "chromedriver.exe")
+	useragent = "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
+	timeout = 10
 
-# Setup
-opts = webdriver.ChromeOptions()
-opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+def PostyUser(Object):
+	address = "1942 E York St, Philadelphia, PA"
 
-chromedriver = os.path.join(os.getcwd(), "chromedriver.exe")
+def PostySpec(Object):
+	home = "https://postmates.com"
+	field = "input"
+	button = "#e2e-go-button"
+	restaurant = "div[role=presentation]"
 
-driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=opts)
+def PostyDriver(Object):
+	config = None
+	user = None
+	spec = None
+	driver = None
 
-# Step One
-driver.get("https://postmates.com/feed")
+	def __init__(self, config=PostyConfig(), user=PostyUser(), spec=PostySpec()):
+		self.config = config
+		self.user = user
+		self.spec = spec
+		self.setup()
 
-# Step Two
-field = driver.find_element_by_css_selector("input")
-field.send_keys("1942 E York St, Philadelphia, PA")
-button = driver.find_element_by_css_selector("#e2e-go-button")
-button.click()
+	def setup(self):
+		opts = webdriver.ChromeOptions()
+		opts.add_argument(self.config.useragent)
+		self.driver = webdriver.Chrome(executable_path=self.config.exe, chrome_options=opts)
 
-try:
-	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[role=presentation]")))
-finally:
-	restaurants = driver.find_elements_by_css_selector("div[role=presentation]")
-	for r in restaurants:
-		print("\n------- RESTAURANT --------")
-		print(r.find_element_by_css_selector("img").get_attribute("alt"))
-		# print(r.get_attribute('innerHTML'))
-		contents = r.get_attribute('innerHTML')
-		regex = "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[\\^'\">\\s]+))?)+\\s*|\\s*)/?>"
-		trimmed = re.sub(regex, "", contents)
-		print(trimmed)
-	driver.quit()
+	def navigateToHome(self):
+		self.driver.get(self.spec.home)
 
+	def fillAddressInHome(self):
+		field = self.driver.find_element_by_css_selector(self.spec.field)
+		field.send_keys(self.user.address)
+
+	def clickAddressInHome(self):
+		button = self.driver.find_element_by_css_selector(self.spec.button)
+		button.click()
+
+	def waitUntilFeedLoads(self):
+		r = None
+		try:
+			r = WebDriverWait(self.driver, self.config.timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, self.spec.restaurant)))
+		finally:
+			return r
+
+	def quit(self):
+		self.driver.quit()
+
+def Posty(Object):
+	driver = None
+	def __init__(self, driver=PostyDriver()):
+		self.driver = driver
+
+	def navigateToHomeAndGetToFeed(self):
+		self.driver.navigateToHome()
+		self.driver.fillAddressInHome()
+		self.driver.clickAddressInHome()
+
+		result = self.driver.waitUntilFeedLoads()
+		restaurants = self.driver.find_elements_by_css_selector("div[role=presentation]")
+		for r in restaurants:
+			print("\n------- RESTAURANT --------")
+			print(r.find_element_by_css_selector("img").get_attribute("alt"))
+			# print(r.get_attribute('innerHTML'))
+			contents = r.get_attribute('innerHTML')
+			regex = "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[\\^'\">\\s]+))?)+\\s*|\\s*)/?>"
+			trimmed = re.sub(regex, "", contents)
+			print(trimmed)
+
+	def quit(self):
+		self.driver.quit()
+
+if __name__ == "__main__":
+	p = Posty()
+	p.navigateToHomeAndGetToFeed()
+	p.quit()
